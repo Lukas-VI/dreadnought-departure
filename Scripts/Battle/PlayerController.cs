a729f86e6d0bfa7fd1e63008ee20cdc79b6bef44
Yanhe06 <luyanhe@hotmail.com>
2026-08-03 15:37:12 +0800

    fix: focus camera on ship body and emphasize selected flag

--- Scripts/Battle/PlayerController.cs
+++ Scripts/Battle/PlayerController.cs
@@ -113,7 +113,7 @@ public partial class PlayerController : Node, IUnitController
 		PreviewNextArrival(ship);
 		bus.EmitSignal("ShipInfoRequested", ship);
 		bus.EmitSignal("ActionSelected", "_show_menu");
-		bus.EmitSignal("CameraTopDownRequested", ShipWorld(ship));
+		bus.EmitSignal("CameraTopDownRequested", ship.GlobalPosition);
 		bus.EmitSignal("LogMessage", $"⚓ 轮到 {ship.ShipName}（剩余 {_pendingShips.Count} 艘）");
 	}
 
